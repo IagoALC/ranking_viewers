31cc63555488846d51e78d1f7e1b4d8d4a3437cd
Iago_infoIdeias <31456590+IagoALC@users.noreply.github.com>
2022-06-28 16:36:01 -0300

    Corrigindo bugs

--- app/Http/Controllers/ViewerWatchTimeController.php
+++ app/Http/Controllers/ViewerWatchTimeController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Models\WatchTime;
 use Illuminate\Support\Facades\DB;
 
 
@@ -14,15 +13,17 @@ class ViewerWatchTimeController extends Controller
         //Fazendo um lista com os canais mais assistidos
         $channelsMostWatched = DB::table("watched_time as wt")
             ->join("channel as ch", "ch.id", "=", "wt.channel_id")
-            ->select(DB::raw("SUM(wt.minutes) as TimeWatched, ch.name"))
+            ->select(DB::raw("SUM(wt.minutes) as timeWatched, ch.name"))
             ->groupBy('ch.id')
+            ->orderBy('timeWatched', 'desc')
             ->get();
 
         //Ranking dos usuários que mais assistem
-        $rankingViewersObject = DB::table("watched_time as wt")
+        $rankingViewers = DB::table("watched_time as wt")
             ->join("user as u", "u.id", "=", "wt.user_id")
-            ->select(DB::raw("u.name, MAX(wt.minutes) as higherWatchedTime, wt.date"))
+            ->select(DB::raw("u.name, MAX(wt.minutes) as higherWatchedTime, SUM(wt.minutes) as timeWatched"))
             ->groupBy('u.id')
+            ->orderBy('timeWatched', 'desc')
             ->get();
 
         //Pegando a data do do dia mais assistido de cada usuário
@@ -30,11 +31,17 @@ class ViewerWatchTimeController extends Controller
             SELECT *, ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY minutes DESC) rn FROM watched_time)
             SELECT id, user_id, channel_id, minutes, date FROM cte WHERE rn = 1 ORDER BY user_id ASC"));
 
-        // // Transforma o objeto em array associativo e depois adiciona a posição do usuário no ranking
-        $rankingViewers = json_decode(json_encode($rankingViewersObject), true);
-        foreach ($rankingViewersObject as $key => $viewer) {
-            $rankingViewers[$key]['position'] = intval($key) + 1;
-            $rankingViewers[$key]['mostWatchedDay'] = $dateMostWatched[$key]->date;
+        //Ajustando ranking dos usuários caso haja tempo assistido igual e adicionando o dia mais assistido de cada usuário
+        $position = 1;
+        for ($i = 0; $i < count($rankingViewers); $i++) {
+            if ($rankingViewers[$i]->timeWatched == ($i < count($rankingViewers) - 1 ? $rankingViewers[$i + 1]->timeWatched : false)) {
+                $rankingViewers[$i]->position = $position;
+                $rankingViewers[$i + 1]->position = $position;
+            } else {
+                $rankingViewers[$i]->position = $position;
+                $position++;
+            }
+            $rankingViewers[$i]->mostWatchedDay = $dateMostWatched[$i]->date;
         }
 
         $response = [
